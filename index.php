e826a0e79f19194515927d9228126799439ce348
777dan <danya.nikitin7@gmail.com>
2022-10-17 19:39:17 +0300

    v 2.0

--- index.php
+++ index.php
@@ -6,9 +6,13 @@ $text =
 <li>Editor</li>
 <li>Subscriber</li>
 </ul>";
-// $replace1 =  preg_replace("#<li>Administrator</li>#", "<li><a href=\"http://www.php.kh.ua/script.php?role=роль\">Administrator</a></li>", $text);
-// $replace2 =  preg_replace("#<li>Editor</li>#", "<li><a href=\"http://www.php.kh.ua/script.php?role=роль\">Editor</a></li>", $replace1);
-echo 
-preg_replace("#<li>Subscriber</li>#", "<li><a href=\"http://www.php.kh.ua/script.php?role=роль\">Subscriber</a></li>", 
-preg_replace("#<li>Editor</li>#", "<li><a href=\"http://www.php.kh.ua/script.php?role=роль\">Editor</a></li>", 
-preg_replace("#<li>Administrator</li>#", "<li><a href=\"http://www.php.kh.ua/script.php?role=роль\">Administrator</a></li>", $text)));
+
+$pattern = '#<li>\w+</li>#';
+$array = ["Administrator", "Editor", "Subscriber"];
+if (preg_match_all($pattern, $text, $arr)) {
+    for ($i=0; $i < func_num_args(); $i++) { 
+        $arrayLower = strtolower($array[$i]);
+        $arr[$i] = "<li><a href='http://www.php.kh.ua/script.php?role={$arrayLower}'>{$array[$i]}</a></li>";
+    }
+}
+print_r($arr);
