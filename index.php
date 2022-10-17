9eb53f8d112cfff50f47310bba82dffbdb48c4fe
777dan <danya.nikitin7@gmail.com>
2022-10-17 20:07:36 +0300

    v 3.0

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
-$text = 
-"<p>Права пользователей:</p>
+$text =
+    "<p>Права пользователей:</p>
 <ul>
 <li>Administrator</li>
 <li>Editor</li>
@@ -8,11 +8,16 @@ $text =
 </ul>";
 
 $pattern = '#<li>\w+</li>#';
-$array = ["Administrator", "Editor", "Subscriber"];
-if (preg_match_all($pattern, $text, $arr)) {
-    for ($i=0; $i < func_num_args(); $i++) { 
-        $arrayLower = strtolower($array[$i]);
-        $arr[$i] = "<li><a href='http://www.php.kh.ua/script.php?role={$arrayLower}'>{$array[$i]}</a></li>";
-    }
-}
-print_r($arr);
+// $array = ["Administrator", "Editor", "Subscriber"];
+// if (preg_match_all($pattern, $text, $arr)) {
+//     for ($i = 0; $i < 3; $i++) {
+//         $arrayLower = strtolower($array[$i]);
+//         $arr[$i] = "<li><a href='http://www.php.kh.ua/script.php?role={$arrayLower}'>{$array[$i]}</a></li>";
+//     }
+// }
+// print_r($arr);
+
+echo 
+preg_replace("#<li>Subscriber</li>#", "<li><a href=\"http://www.php.kh.ua/script.php?role=роль\">Subscriber</a></li>", 
+preg_replace("#<li>Editor</li>#", "<li><a href=\"http://www.php.kh.ua/script.php?role=роль\">Editor</a></li>", 
+preg_replace("#<li>Administrator</li>#", "<li><a href=\"http://www.php.kh.ua/script.php?role=роль\">Administrator</a></li>", $text)));
