4549fa817fb14af4487348889ee3b7a690a55fd7
777dan <danya.nikitin7@gmail.com>
2022-10-23 12:52:01 +0300

    v 3.0

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-$text = "<p>Права пользователей:</p><ul><li>Administrator</li><li>Editor</li><li>Subscriber</li></ul>";
-$pattern = '#(<li>\w+</li>)(<li>\w+</li>)(<li>\w+</li>)#';
-$replacement = 
-'${1}<li><a href=\"http://www.php.kh.ua/script.php?role=administrator\">Administrator</a></li>
-${2}<li><a href=\"http://www.php.kh.ua/script.php?role=editor\">Editor</a></li>
-${3}<li><a href=\"http://www.php.kh.ua/script.php?role=subscriber\">Subscriber</a></li>';
-echo preg_replace($pattern, $replacement, $text);
+$text = "<p>Права пользователей:</p>
+<ul>
+<li>Administrator</li>
+<li>Editor</li>
+<li>Subscriber</li>
+</ul>";
+echo preg_replace('#<li>(\w+)</li>#', "<li><a href=\"http://www.php.kh.ua/script.php?role=$1\">$1</a></li>", $text);
